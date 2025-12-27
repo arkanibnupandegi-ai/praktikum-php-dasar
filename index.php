6743b31ddcf1f333eeeff2fb2ed2784757a22b3e
arkanibnupandegi-ai <arkanibnupandegi@gmail.com>
2025-12-27 11:06:03 +0700

    Tugas 3 Praktikum PHP Dasar

---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+echo "<h1>Praktikum PHP Dasar</h1>";
+echo "<p>Nama: Arkan Ibnu Pandegi</p>";
+echo "<p>NPM: 4522210097</p>";
+?>
---
+++ variabel.php
@@ -0,0 +1,7 @@
+<?php
+$nama = "Arkan Ibnu Pandegi";
+$npm = "4522210097";
+
+echo "Nama: $nama <br>";
+echo "NPM: $npm";
+?>
